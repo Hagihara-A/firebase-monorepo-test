da4e2ea8843692cea7e864ca3d11763bf7458e5b
arark <knkr5456@gmail.com>
2022-05-22 17:40:29 +0900

    ignore build/**

--- libs/util/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.splitArray = void 0;
-const splitArray = (arr, n) => {
-    const groupLen = Math.ceil(arr.length / n);
-    return [...Array(groupLen)].map((_, i) => arr.slice(i * n, (i + 1) * n));
-};
-exports.splitArray = splitArray;
