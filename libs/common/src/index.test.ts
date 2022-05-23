c702a28d077f1eb263edab46c0b60cb7ec7e3596
Arark <knkr5456@gmail.com>
2022-05-24 01:31:40 +0900

    feat(libs/common): impl sayHelloMonorepo

--- libs/common/src/index.test.ts
+++ libs/common/src/index.test.ts
@@ -1,8 +1,7 @@
-import { splitArray } from ".";
+import { sayHelloMonorepo } from ".";
 
-test(`${splitArray.name}`, () => {
-  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const actual = splitArray(arr, 3);
-  const expected = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]];
-  expect(actual).toEqual(expected);
+test(`${sayHelloMonorepo.name}`, () => {
+  const actual = sayHelloMonorepo();
+  const expected = "hello";
+  expect(actual).not.toEqual(expected);
 });
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,4 +1,1 @@
-export const splitArray = <T>(arr: T[], n: number) => {
-  const groupLen = Math.ceil(arr.length / n);
-  return [...Array(groupLen)].map((_, i) => arr.slice(i * n, (i + 1) * n));
-};
+export const sayHelloMonorepo = () => "Hello monorepo!";
